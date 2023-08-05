a358ac742d34990c55a32043d99c5796285bcc0d
juarezrbr1 <juarezribeiro94@gmail.com>
2023-08-05 15:10:10 -0300

    Aula 66

--- recipes/views.py
+++ recipes/views.py
@@ -1,24 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from utils.recipes.factory import make_recipe
+from django.http import Http404
+
 from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-    })
-
-def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        is_published=True
     ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
+
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True).order_by('-id')
+        )
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
+    })
+
+
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe,
+                               pk=id,
+                               is_published=True)
+    
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
-        'is_datail_page': True,
+        'recipe': recipe,
+        'is_detail_page': True,
     })
