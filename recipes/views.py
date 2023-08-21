707b9791c4d70693072fc4e5dff7800fef67d307
juarezrbr1 <juarezribeiro94@gmail.com>
2023-08-20 22:36:18 -0300

    update

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from utils.recipes.factory import make_recipe
-from django.http import Http404
+from django.db.models import Q
 from .models import Recipe
 from django.http.response import Http404
 
@@ -36,9 +35,21 @@ def recipe(request, id):
     })
 
 def search(request):
-    search_term = request.GET.get('q')
+    search_term = request.GET.get('q', '').strip()
 
     if not search_term:
         raise Http404()
+    
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) | 
+            Q(description__icontains=search_term)
+        ),
+        is_published=True,
+    ).order_by('-id')
 
-    return render(request, 'recipes/pages/search.html')
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Seach for "{search_term } |"',
+        'search_term': search_term,
+        'recipes': recipes,
+    })
