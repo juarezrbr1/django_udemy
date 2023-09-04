eb650ab96473f8e79a788e238874df74b8da2109
juarezrbr1 <juarezribeiro94@gmail.com>
2023-09-04 00:49:01 -0300

    update

--- recipes/admin.py
+++ recipes/admin.py
@@ -6,8 +6,10 @@ from .models import Category, Recipe
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     ...
+
 
 admin.site.register(Category, CategoryAdmin)
