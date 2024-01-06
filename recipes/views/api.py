e1636c867288ec55c9b09409fca28150775d4cef
juarezrbr1 <juarezribeiro94@gmail.com>
2024-01-06 18:19:44 -0300

    aula 299

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from tag.models import Tag
 from rest_framework import status
 from ..models import Recipe
@@ -12,13 +12,7 @@ from ..serializers import RecipeSerializer, TagSerializer
 class RecipeAPIv2Pagination(PageNumberPagination):
     page_size = 2
 
-class RecipeAPIv2List(ListCreateAPIView):
-    queryset = Recipe.objects.get_published()
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeAPIv2Pagination
-
-
-class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
+class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
